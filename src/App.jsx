dd8cdd3898dc811c668fc77bd47d5121b3fe9ce6
Bino Bense <binobense075@gmail.com>
2025-10-02 22:25:58 +0530

    path changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
 import Login from "./components/Login";
-import Profile from "./components/profile";
+import Profile from "./components/Profile";
 import { Provider } from "react-redux";
 import store from "./utils/appStore";
 import Feed from "./components/Feed";
